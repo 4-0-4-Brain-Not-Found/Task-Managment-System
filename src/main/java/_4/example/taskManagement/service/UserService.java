ed0a003e6440c1e3bd6b862ae5793fb6d62c3e87
Zeynep Ö. <116917341+zeyn-zeze@users.noreply.github.com>
2024-12-13 15:07:28 +0300

    spring.session and userService settings

--- src/main/java/_4/example/taskManagement/service/UserService.java
+++ src/main/java/_4/example/taskManagement/service/UserService.java
@@ -5,7 +5,6 @@ import _4.example.taskManagement.entities.users.User;
 import _4.example.taskManagement.enums.Role;
 import _4.example.taskManagement.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,36 +15,40 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
 
     public User registerUser(RegisterDTO userRegisterDTO) {
 
+        // Kullanıcı adı kontrolü
         if (userRepository.findByUsername(userRegisterDTO.getUsername()).isPresent()) {
             throw new RuntimeException("Username is already taken");
         }
 
+        // E-posta kontrolü
         if (userRepository.findByEmail(userRegisterDTO.getEmail()).isPresent()) {
             throw new RuntimeException("Email is already registered");
         }
 
+        // Yeni kullanıcı oluşturulur
         User user = new User();
         user.setUsername(userRegisterDTO.getUsername());
         user.setEmail(userRegisterDTO.getEmail());
-        user.setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
+        user.setPassword(userRegisterDTO.getPassword());
         user.setPhoneNo(userRegisterDTO.getPhoneNumber());
         user.setRole(Role.ROLE_USER);
 
+        // Kullanıcıyı veritabanına kaydet
         return userRepository.save(user);
     }
 
     public Optional<User> loginUser(String username, String password) {
         Optional<User> user = userRepository.findByUsername(username);
 
-        if (user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())) {
+        //Şifre doğrulanması
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
             return user;
         } else {
-            throw new RuntimeException("Invalid Username or Password ! Try Again ");
+            throw new RuntimeException("Invalid Username or Password! Try Again");
         }
     }
 }
